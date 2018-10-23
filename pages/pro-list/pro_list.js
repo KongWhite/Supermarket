8a11615206c01a59b35d00d518ad4a9bc72cb0d3
KongWhite <694607380@qq.com>
2018-10-23 17:34:43 +0800

    1.1.0 from haorui

--- pages/pro-list/pro_list.js
+++ pages/pro-list/pro_list.js
@@ -1,18 +1,57 @@
 // pages/pro-list/pro_list.js
 Page({
-
+  tab:function(e){
+    var type = e.target.dataset.type;
+    //this.data.num = e.target.dataset.num;
+    this.setData({
+      num:e.target.dataset.num
+    })
+    wx.request({
+      url: 'http://176.137.16.31:3000/products/list?type='+type,
+      success:(result)=>{
+        //console.log(result.data);
+        this.setData({
+          pro_items:result.data
+        })        
+      }
+    })
+  },
+  detail:function(e){
+    var pid = e.target.dataset.pid;
+    console.log(pid);
+    wx.navigateTo({
+      url: '../pro-detail/pro_detail?pid='+pid
+    })
+  },
   /**
    * 页面的初始数据
    */
-  data: {
-
+  data:{
+    num:0,
+    pro_list:[],
+    pro_items:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.request({
+      url: 'http://176.137.16.31:3000/products/list-name',
+      success:(result)=>{
+        this.setData({pro_list:result.data})
+        console.log(this.data.pro_list);
+      }
+    })
+    wx.request({
+      url: 'http://176.137.16.31:3000/products/list?type=1001',
+      success: (result) => {
+        this.setData({
+          pro_items: result.data
+        })
+      }
+    })
+    
   },
 
   /**
