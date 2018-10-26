416023983110273baa5ed4adf47cbda34cde9fb3
Arrowfield <768449566@qq.com>
2018-10-26 13:21:01 +0800

    将二级类表的数据通过request请求回来

--- pages/pro/pro.js
+++ pages/pro/pro.js
@@ -1,51 +1,33 @@
 // miniprogram/pages/component/pro/pro.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-<<<<<<< HEAD
-    type:0
-
-=======
-    pros:[]
->>>>>>> c75f92545f5589dfb9616ebd8355ffd9382f0060
+    type:0,
+    typeList:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-<<<<<<< HEAD
-    //接收传过来的参数
-    //通过链接传参的方式获取参数
-    console.log(options);
+    ///supProducts/list?type=type
+    //url:"https://myserver.applinzi.com/supIndex/swiper",
     var type = options.type;
     //通过商品类别名称的编号查询该类别下是商品
-    this.setDate({
+    this.setData({
       type:type
-=======
-    var type = options.type;
-    console.log(options);
-    wx.request({
-      url: 'https://myserver.applinzi.com/supProducts/list?type='+type,
-      success:(result)=>{
-        //console.log(result);
-        this.setData({
-          pros:result.data
-        })
-      }
     })
     wx.request({
-      url: 'https://myserver.applinzi.com/supProducts/list?type=' + type,
-      success: (result) => {
-        //console.log(result);
+      url:"https://myserver.applinzi.com/supProducts/list?type="+this.data.type,
+      method:"GET",
+      success:(res)=>{
+        console.log(res);
         this.setData({
-          pros: result.data
+          typeList:res.data
         })
       }
->>>>>>> c75f92545f5589dfb9616ebd8355ffd9382f0060
     })
   },
 
